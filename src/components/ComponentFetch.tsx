a7349693e1a8bd6e8d29800be64755062f86a070
David Johnston <david.johnston@coles.com.au>
2024-08-19 16:54:25 +1000

    White space clean up

--- src/components/ComponentFetch.tsx
+++ src/components/ComponentFetch.tsx
@@ -2,23 +2,14 @@
 
 import React from "react";
 
-export type ComponentFetchProps = {
-
-}
-
-export function ComponentFetch(props: ComponentFetchProps) {
-
-
+export function ComponentFetch() {
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts/1").then((res) => res.json()).then((d) => setData(d))
     }, [])
 
-
     return <div>
-
         {JSON.stringify(data)}
     </div>
-
 } 
--- src/components/ComponentFetchRelative.tsx
+++ src/components/ComponentFetchRelative.tsx
@@ -1,24 +1,13 @@
 "use client";
-
 import React from "react";
 
-export type ComponentFetchProps = {
-
-}
-
-export function ComponentFetchRelative(props: ComponentFetchProps) {
-
-
+export function ComponentFetchRelative() {
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
         fetch("/api/posts/2").then((res) => res.json()).then((d) => setData(d))
     }, [])
-
-
     return <div>
-
         {JSON.stringify(data)}
     </div>
-
 } 
