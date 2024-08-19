13bbe729ed1ad6826a3557e63a909b208bbface9
David Johnston <david.johnston@coles.com.au>
2024-08-19 17:16:09 +1000

    Add proxying back in

--- src/app/page6/page.tsx
+++ src/app/page6/page.tsx
@@ -1,7 +1,12 @@
+import {HttpsProxyAgent} from 'https-proxy-agent';
+
 import nodeFetch from"node-fetch";
+
+const agent = new HttpsProxyAgent("https://127.0.0.1:8080")
 
 async function getData() {
     const result = await nodeFetch("https://jsonplaceholder.typicode.com/posts/6", {
+        agent: agent
     })
     return result.json();
 }
